845014d7659d8b4c708f026f1f4ddace01d3fb91
Harel <harels55@gmail.com>
2019-06-03 18:56:55 +0300

    harel is the best
    and bibi is the king

--- src/DownloadSystem.java
+++ src/DownloadSystem.java
@@ -37,24 +37,5 @@ public class DownloadSystem implements State {
         return offState;
     }
 
-    public static void main(String[] args) {
-        while(true) {
-            System.out.println("Tell the system what to do :");
-            Scanner userInput = new Scanner(System.in);
-
-            while(!userInput.hasNext());
-
-            String input = "";
-            if (userInput.hasNext()) input = userInput.nextLine();
-
-            System.out.println("input is '" + input + "'");
-
-            if (!input.equals("")) {
-                //main code
-            }
-            userInput.close();
-            ..Thread.sleep(1000);
-        }
-
-    } //End of main
+//End of main
 }
